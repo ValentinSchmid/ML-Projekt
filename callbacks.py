c2365ce42df177b4f7d9d6ae0afe2ecd259ec5ba
ValentinSchmid <45970251+ValentinSchmid@users.noreply.github.com>
2019-03-16 14:22:08 +0100

    neu

--- callbacks.py
+++ callbacks.py
@@ -3,31 +3,25 @@ import random
 from random import shuffle
 from time import time, sleep
 from collections import deque
+import os.path as op
 
 from settings import s
 from settings import e
 
 def setup(self):
-    np.random.seed()
-    
-    arena = self.game_state['arena']
-    accessible = []
-    for d in arena:
-        if (arena[d] != 1):
-            accessible.append(d)
+    np.random.seed()    
     # Q matrix
-    Q = np.matrix(np.zeros([len(accessible),5]))  # [][0] = up ; [][1] = down ; [][2] = left ; [][3] = right ; [][4] = wait
-
-
-def act(self):
-    self.logger.info('Pick action at random')
+    if op.isfile("Q.txt") != True:        
+        Q=np.zeros((176,5),dtype=int)
+        np.savetxt("Q.txt", Q)
+def act(self):    
+    Q = np.loadtxt("Q.txt")
+    arena = self.game_state['arena']
     x, y, _, bombs_left, score = self.game_state['self']
-    state = (x,y)
-    
+    state = (x,y)    
     epsilon = 0.7    
     action_ideas = ['UP', 'DOWN', 'LEFT', 'RIGHT' , 'WAIT']
-    shuffle(action_ideas)
-        
+    shuffle(action_ideas)        
     if np.random.rand(1) <= epsilon:
         self.next_action = action_ideas.pop()
     else:
@@ -37,13 +31,11 @@ def act(self):
         if act == 1: action_ideas.append('DOWN')
         if act == 2: action_ideas.append('LEFT')
         if act == 3: action_ideas.append('RIGHT')
-        if act == 4: action_ideas.append('WAIT')
-        
+        if act == 4: action_ideas.append('WAIT')        
         self.next_action = action_ideas.pop()
+    self.logger.info('Pick action at random')
 
-def reward_update(self):
-    self.logger.debug(f'Encountered {len(self.events)} game event(s)')
-    
+def reward_update(self):   
     reward[state][self.next_action] = 0
     if self.events == e.MOVED_LEFT : reward[state][self.next_action] =  reward[state][self.next_action] - 1
     if self.events == e.MOVED_RIGHT : reward[state][self.next_action] =  reward[state][self.next_action] - 1
@@ -53,18 +45,19 @@ def reward_update(self):
     if self.events == e.COIN_COLLECTED : reward[state][self.next_action] =  reward[state][self.next_action] + 100
     if self.events == e.INVALID_ACTION : reward[state][self.next_action] =  reward[state][self.next_action] - 100
     print('State:',state,'Reward:',reward[state][self.next_action])
+    self.logger.debug(f'Encountered {len(self.events)} game event(s)')
 
-def end_of_episode(self):
-    self.logger.debug(f'Encountered {len(self.events)} game event(s) in final step')
-    
+def end_of_episode(self):  
+    Q = np.loadtxt("Q.txt")
     alpha = 1
-    gamma = 0.9
-    
+    gamma = 0.9    
     for state in accessible:
         if self.next_action == 'UP': next_state = (x,y+1)
         if self.next_action == 'DOWN': next_state =  (x,y-1)
         if self.next_action == 'LEFT': next_state =  (x-1,y)
         if self.next_action == 'RIGHT': next_state = (x+1,y)
         if self.next_action == 'WAIT': next_state = (x,y)
-        max_Q_next = np.argmax(Q[next_state])
-        Q[state][self.next_action] = Q[state][self.next_action] + alpha * (reward[state][self.next_action] + gamma * max_Q_next - Q[state][self.next_action])
+#        max_Q_next = np.argmax(Q[next_state])    
+#    Q[state][self.next_action] = Q[state][self.next_action] + alpha * (reward[state][self.next_action] + gamma * max_Q_next - Q[state][self.next_action])
+    print(Q)
+    self.logger.debug(f'Encountered {len(self.events)} game event(s) in final step')
