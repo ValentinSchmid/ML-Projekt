81528d9b93178082b08767625bfc6eca331f4a67
ValentinSchmid <45970251+ValentinSchmid@users.noreply.github.com>
2019-03-20 13:07:21 +0100

    Add files via upload

--- callbacks.py
+++ callbacks.py
@@ -14,24 +14,17 @@ import sklearn.preprocessing
 from sklearn.linear_model import SGDRegressor
 from sklearn.kernel_approximation import RBFSampler
 
-def statefun(arena,agent_row,agent_col,coins):   
-    accessible = []
-    for a in range(16):
-        for b in range(16):
-            if (arena[a][b] != -1):
-                accessible.append((a,b))    
-    state=accessible.index((agent_row,agent_col))
-    for i in range(len(coins)):
-        state = np.append(state,accessible.index((coins[i][0],coins[i][1])))
-    return state
 
 def setup(self):
     np.random.seed()    
     # Q matrix
     if op.isfile("Q.txt") != True:        
-        Q = np.zeros((12,5),dtype=float)
+        Q = np.zeros((12,5),dtype = float)
         np.savetxt("Q.txt", Q)
-    self.coordinate_history = deque([], 20)
+    self.coordinate_history = deque([], 400)
+    self.pick_history = deque([], 400)
+    self.states_history = deque([], 400)
+    self.index_history = deque([], 400)
     self.logger.info('Initialize')
     
 def act(self):    
@@ -41,7 +34,7 @@ def act(self):
     x, y, _, bombs_left, score = self.game_state['self']
     self.coordinate_history.append((x,y))
 
-    epsilon = 0.0
+    epsilon = 0.7
            
     if np.random.rand(1) <= epsilon:
         action_ideas = ['UP','DOWN','RIGHT','LEFT','WAIT']
@@ -53,56 +46,65 @@ def act(self):
         
         f1 = x
         f2 = y
-        states = np.array([1,f1,f2])
+        states = np.array([f2,f1,1])
         for i in range(len(coins)):
-            states=np.append(states,(np.abs(coins[i][0]-x)+np.abs(coins[i][1]-y)))
+            states = np.append(states,(np.abs(coins[i][0] - x) + np.abs(coins[i][1] - y)))
+        if len(states) < 12:
+            for i in range(12 - len(states)):
+                states = np.append(states , 0 )
             
-        #d1 = np.abs(coins[1][0] - x) + np.abs(coins[1][1] - y)
-        #d2 = np.abs(coins[2][0] - x) + np.abs(coins[2][1] - y)
-        #d3 = np.abs(coins[3][0] - x) + np.abs(coins[3][1] - y)
-        #d4 = np.abs(coins[4][0] - x) + np.abs(coins[4][1] - y)
-        #d5 = np.abs(coins[5][0] - x) + np.abs(coins[5][1] - y)
-        #d6 = np.abs(coins[6][0] - x) + np.abs(coins[6][1] - y)
-        #d7 = np.abs(coins[7][0] - x) + np.abs(coins[7][1] - y)
-        #d8 = np.abs(coins[8][0] - x) + np.abs(coins[8][1] - y)
-        #d9 = np.abs(coins[9][0] - x) + np.abs(coins[9][1] - y)
-        print(Q)
-        print(states)
         act = np.argmax( (Q.transpose()).dot(states) )
-        #action_ideas=[]
-        #if act == 0: action_ideas.append('UP')
-        #if act == 1: action_ideas.append('DOWN')
-        #if act == 2: action_ideas.append('LEFT')
-        #if act == 3: action_ideas.append('RIGHT')
-        #if act == 4: action_ideas.append('WAIT')        
-        #self.next_action = action_ideas.pop()
+        if act == 0: action_ideas.append('UP')
+        if act == 1: action_ideas.append('DOWN')
+        if act == 2: action_ideas.append('LEFT')
+        if act == 3: action_ideas.append('RIGHT')
+        if act == 4: action_ideas.append('WAIT')        
+        self.next_action = action_ideas.pop()
+    self.states_history.append((1))
+    self.pick_history.append(self.next_action)
     self.logger.info('Pick action at random')
     
 def reward_update(self):   
-    Q = np.loadtxt("Q.txt")
     arena = self.game_state['arena']
     coins = self.game_state['coins']
     x, y, _, bombs_left, score = self.game_state['self']
-    state = statefun(arena,x,y,coins)
-    index = 4
-    next_state=np.array([x,y])
+    Q=np.loadtxt("Q.txt")
+    # reward matrix
+    if (op.isfile("rewards.txt") == False):
+        rewards = np.zeros((12,5),dtype = float)
+        np.savetxt("rewards.txt", rewards)
+    else:
+        rewards = np.loadtxt("rewards.txt")
+    f1 = x
+    f2 = y
+    states = np.array([f2,f1,1])
+    for i in range(len(coins)):
+        states = np.append(states,(np.abs(coins[i][0] - x) + np.abs(coins[i][1] - y)))
+    if len(states) < 12:
+        for i in range(12 - len(states)):
+            states = np.append(states , 0 )
+    
     if self.events[0] == e.MOVED_LEFT: 
         next_state = np.array([x-1,y])
+        reward = -1
     if self.events[0] == e.MOVED_RIGHT: 
         next_state =  np.array([x+1,y])
+        reward = -1
     if self.events[0] == e.MOVED_UP: 
         next_state = np.array([x,y+1])
+        reward = -1
     if self.events[0] == e.MOVED_DOWN: 
         next_state = np.array([x,y-1])
+        reward = -1
     if self.events[0] == e.WAITED: 
         next_state= np.array([x,y])
+        reward = -1
     if len(self.events)>1:
         if self.events[1] == e.COIN_COLLECTED:
-            reward=100
-            next_coins = coins[:-1]            
+            reward = 100
     else:
-            reward=-1
-            next_coins = coins
+        reward = -10
+
             
     index = 0
     if self.next_action == 'UP':
@@ -115,19 +117,24 @@ def reward_update(self):
         index = 3
     if self.next_action == 'WAIT':
         index = 4
-    Next_state = statefun(arena,next_state[0],next_state[1],next_coins)
-    Q[state][index]=(1-alpha)*Q[state][index]+alpha*(reward+ np.argmax(Q[Next_state]))
     
-    np.savetxt("Q.txt", Q)    
+    self.index_history.append(index)
     
+    alpha = 0.3
+    for i in range(12):
+        rewards[i][index] = (1-alpha)*reward - alpha*((Q.transpose()).dot(states))[index]
+    if rewards[1][index]>1000:
+        print((Q.transpose()).dot(states))
+        print(reward)
+    
+    np.savetxt("rewards.txt", rewards)    
     
     
 def end_of_episode(self):  
     Q = np.loadtxt("Q.txt")
+    rewards = np.loadtxt("rewards.txt")
     arena = self.game_state['arena']
     coins = self.game_state['coins']
-    alpha = 1
-    gamma = 0.9    
     if op.isfile("coins_algo.txt") != True:
         collected = 9-len(coins)
         np.savetxt("coins_algo.txt",collected)
@@ -135,12 +142,17 @@ def end_of_episode(self):
         collected = np.loadtxt("coins_algo.txt")
         collected = np.append(collected,9-len(coins))
         np.savetxt("coins_algo.txt",collected)
-   # for state in accessible:
-   #     if self.next_action == 'UP': next_state = (x,y+1)
-   #     if self.next_action == 'DOWN': next_state =  (x,y-1)
-   #     if self.next_action == 'LEFT': next_state =  (x-1,y)
-   #     if self.next_action == 'RIGHT': next_state = (x+1,y)
-   #     if self.next_action == 'WAIT': next_state = (x,y)
-   #     max_Q_next = np.argmax(Q[next_state])    
-   # Q[state][self.next_action] = Q[state][self.next_action] + alpha * (reward[state][self.next_action] + gamma * max_Q_next - Q[state][self.next_action])  
+    
+    alpha = 0.3
+    beta = 0.2
+    
+    for k in range(len(self.coordinate_history)):
+        state = self.states_history.popleft()
+        next_state = self.states_history[0]
+        index = self.index_history.popleft()
+        #for i in range(12):
+         #   Q[i][index] = Q[i][index] + alpha * (rewards[i][index] + beta * np.argmax((Q.transpose()).dot(next_state))) * state[i]
+    
+    np.savetxt("Q.txt", Q)
+    
     self.logger.debug(f'Encountered {len(self.events)} game event(s) in final step')
